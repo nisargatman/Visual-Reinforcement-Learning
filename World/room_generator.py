a42ad5fe58589073900bec975d82268b58bef1d2
Nisarg Mehta <nm555@cam.ac.uk>
2016-11-06 03:02:17 +0000

    added config dict

---
+++ World/room_generator.py
@@ -0,0 +1,27 @@
+import MalmoPython
+import os
+import sys
+import time
+
+config = {
+    'length':50,
+    'breadth':50,
+    'height':20,
+    'ceiling':"yes",
+    'num_torches':5,
+    'torch_pos':(1,2,3,4,5),
+    'num_chairs':1,
+    'chair_pos':(1),
+    'height_bookcase':10,
+    'bookcase_pos':8,
+    'num_lamps':1,
+    'lamp_pos':(2),
+    'nun_sofas':1,
+    'sofa_pos':(2),
+    'num_tables':1,
+    'table_pos':(1),
+    'cakes':"yes",
+    'cake_pos':(1)
+
+}
+
