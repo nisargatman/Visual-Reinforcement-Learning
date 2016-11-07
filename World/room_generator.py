97865020a36282c55048a48362c20057956a2c7a
Nisarg Mehta <nm555@cam.ac.uk>
2016-11-07 02:50:25 +0000

    Edited xml, get frames

--- World/room_generator.py
+++ World/room_generator.py
@@ -2,6 +2,7 @@ import MalmoPython
 import os
 import sys
 import time
+import random
 
 config = {
     'length':50,
@@ -26,23 +27,23 @@ config = {
 } # dictionary that contains configuraiton for setting up world
 
 def build_world(my_mission):
-    for i in range(config['length']):
-        for j in range(config['breadth']):
-            pass
+    pass
 
 def main():
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-
     agent_host = MalmoPython.AgentHost()
 
-    mission_file = 'xmls/mission_xml.xml'
+    mission_file = './xmls/mission_xml.xml'
     with open(mission_file, 'r') as f:
         print "Loading mission from %s" % mission_file
-        missionXML = f.read()
+        mission_xml = f.read()
+    my_mission = MalmoPython.MissionSpec(mission_xml,True)
 
-    my_mission = MalmoPython.MissionSpec(missionXML, True)
-    my_mission_record = MalmoPython.MissionRecordSpec("./saved_data.tgz")
+    build_world(my_mission)
+
     my_mission.requestVideo( 640, 480 )
+
+    my_mission_record = MalmoPython.MissionRecordSpec()
 
     max_retries = 3
     for retry in range(max_retries):
@@ -69,6 +70,8 @@ def main():
     # main loop:
     while world_state.is_mission_running:
         agent_host.sendCommand( "move 1" )
+        agent_host.sendCommand( "turn " + str(random.random()*2-1) )
+        agent_host.sendCommand("move 1")
         time.sleep(0.5)
         world_state = agent_host.getWorldState()
         print "video,observations,rewards received:",world_state.number_of_video_frames_since_last_state,world_state.number_of_observations_since_last_state,world_state.number_of_rewards_since_last_state
@@ -78,8 +81,9 @@ def main():
             print "Error:",error.text
         for frame in world_state.video_frames:
             print "Frame:",frame.width,'x',frame.height,':',frame.channels,'channels'
+            print type(frame.pixels)
             #image = Image.frombytes('RGB', (frame.width, frame.height), str(frame.pixels) ) # to convert to a PIL image
     print "Mission has stopped."
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
